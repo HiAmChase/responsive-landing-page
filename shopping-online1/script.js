f82eb627e4b86bd6bc1be952fa3dbc2fdd4b612d
HiAmChase <ngocthinh303@gmail.com>
2021-03-17 08:36:12 +0700

    update file

--- shopping-online1/script.js
+++ shopping-online1/script.js
@@ -4,7 +4,7 @@ const toggleBtn = document.querySelector(".toggle");
 window.addEventListener('scroll', () => {
     header.classList.toggle('sticky', window.scrollY > 0);
 });
-
+    
 toggleBtn.addEventListener('click', toggle);
 
 function toggle() {
